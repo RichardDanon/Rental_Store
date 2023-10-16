a05b0b3114a9a01aa7cdf18fecc0c8d80c88c9c7
test <test>
2023-10-16 09:49:28 -0400

    Initial Commit

---
+++ Rental_Store/Rental_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rental_StoreApp.swift
+//  Rental_Store
+//
+//  Created by macuser on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct Rental_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
