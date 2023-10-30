eef175a8b0374d567678b89bd10fe2e5a76e3e2e
test <test>
2023-10-30 09:57:41 -0400

    Create page

--- Rental_Store/ViewModels/RentingViewModel.swift
+++ Rental_Store/ViewModels/RentingViewModel.swift
@@ -15,3 +15,115 @@ class RentingViewModel: ObservableObject {
         // Initialize your mock data here
     }
 }
+
+// Equipement View
+class EquipementViewModel {
+    var equipmentGroups: [EquipmentGroup] = []
+
+    // Fetch equipment groups from your model.
+    func fetchEquipmentGroups() {
+        // Implement fetching logic here and populate equipmentGroups.
+    }
+
+    // Create a new equipment group.
+    func createEquipmentGroup(groupName: String) {
+        // Implement creation logic and add the new group to equipmentGroups.
+    }
+
+    // Delete an equipment group.
+    func deleteEquipmentGroup(at index: Int) {
+        // Implement deletion logic and remove the group at the specified index.
+    }
+
+    // Navigate to EquipmentDetails.
+    func navigateToEquipmentDetails(equipment: Equipment) {
+        // Implement navigation logic to EquipmentDetails view.
+    }
+
+    // Navigate to CreateGroup.
+    func navigateToCreateGroup() {
+        // Implement navigation logic to CreateGroup view.
+    }
+}
+
+
+//Equipement Details
+class EquipementDetailsViewModel {
+    var selectedEquipment: Equipment?
+
+    // Display availability and usages of selected equipment.
+    func displayAvailability() -> Availability {
+        return selectedEquipment?.availability ?? .free
+    }
+
+    func displayUsages() -> [Usage] {
+        return selectedEquipment?.usages ?? []
+    }
+
+    // Update availability and usages.
+    func updateAvailability(newAvailability: Availability) {
+        // Implement updating logic for availability.
+    }
+
+    func updateUsages(newUsages: [Usage]) {
+        // Implement updating logic for usages.
+    }
+
+    // Navigate back to EquipementView.
+    func navigateBack() {
+        // Implement navigation logic to return to EquipementView.
+    }
+}
+
+//User View
+class UserViewModel {
+    var users: [User] = []
+
+    // Fetch user data from your model.
+    func fetchUserData() {
+        // Implement fetching user data logic and populate users.
+    }
+
+    // Navigate to UserDetails.
+    func navigateToUserDetails(user: User) {
+        // Implement navigation logic to UserDetails view.
+    }
+}
+
+// User Details View
+class UserDetailsViewModel {
+    var selectedUser: User?
+
+    // Display user details and rented items.
+    func displayUserDetails() -> (phoneNumber: String, email: String, isRenting: Bool) {
+        let user = selectedUser
+        return (user?.phoneNumber ?? "", user?.email ?? "", user?.isRenting ?? false)
+    }
+
+    func displayRentedItems() -> [Equipment] {
+        return selectedUser?.rentingItems ?? []
+    }
+
+    // Update the user's renting status.
+    func updateRentingStatus(isRenting: Bool) {
+        // Implement updating logic for the user's renting status.
+    }
+
+    // Navigate back to UserView.
+    func navigateBack() {
+        // Implement navigation logic to return to UserView.
+    }
+}
+
+//Create group view
+class CreateGroupViewModel {
+    // Create a new equipment group in the model.
+    func createNewGroup(groupName: String) {
+        // Implement group creation logic in the model.
+    }
+
+    // Navigate back to EquipementView.
+    func navigateBack() {
+        // Implement navigation logic to return to EquipementView.
+    }
+}
--- Rental_Store/Views/CreateGroup.swift
+++ Rental_Store/Views/CreateGroup.swift
@@ -1,27 +1,52 @@
-//
-//  CreateGroup.swift
-//  Rental_Store
-//
-//  Created by Richard on 2023-10-30.
-//
-
-import SwiftUI
-
 import SwiftUI
 
 struct CreateGroup: View {
-
+    @State private var groupName = ""
+    
     var body: some View {
-        VStack {
-            Text("Create Group")
-            // Implement an input field for group name
-            Button("Create") {
-                // Create the group and dismiss the view
+        GeometryReader { geometry in
+            ZStack {
+                Color(UIColor.systemBackground)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Text("Create New Group")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.blue)
+                        .padding(.bottom, 20)
+                    
+                    TextField("Group Name", text: $groupName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal, 20)
+                    
+                    
+                    Button(action: {
+                        // Implement your logic for creating a new group with groupName
+                        // Then navigate back to EquipementView.
+                    }) {
+                        Text("Submit")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 50)
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    
+                    Button(action: {
+                        // Implement your logic for navigating back to EquipementView without creating a group.
+                    }) {
+                        Text("Dismiss")
+                    }
+                    .padding(.top, 10)
+                    .padding(.horizontal, 20)
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height)
             }
         }
     }
 }
-
 
 struct CreateGroup_Previews: PreviewProvider {
     static var previews: some View {
